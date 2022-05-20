af4e4c5a9a414a446fe5bbc801ff5dc25f932862
Dugong123 <muhammaddhanizakariya12@gmail.com>
2022-05-20 22:03:47 +0700

    Penambahan layout setting, camera, profil, result, select image

---
+++ app/src/main/java/com/dicoding/picodiploma/besti/view/selectimage/SelectImageActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.picodiploma.besti.view.selectimage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_image)
+    }
+}
---
+++ app/src/main/java/com/dicoding/picodiploma/besti/view/setting/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.besti.view.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.picodiploma.besti.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
